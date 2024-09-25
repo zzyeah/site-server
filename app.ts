a19fec45a8a4efb13926147347c232adba1162b4
zzyeah <541767316@qq.com>
2024-09-26 03:07:49 +0800

    feat: :zap: fix cors
    添加cors依赖,处理跨域问题

--- app.ts
+++ app.ts
@@ -6,6 +6,7 @@ import express, { ErrorRequestHandler } from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import cors from 'cors';
 // import historyApiFallback from "connect-history-api-fallback";
 
 // import sql
@@ -45,6 +46,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use(
   expressjwt({
